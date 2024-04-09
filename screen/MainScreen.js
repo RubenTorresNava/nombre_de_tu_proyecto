d710f142656bef88441183959bc69d7a4e713c92
Ruben Torres <rubenbebe.145.rt@gmail.com>
2024-04-08 22:05:39 -0600

    Agregados estilos al componente MainScreen.js

--- screen/MainScreen.js
+++ screen/MainScreen.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, StyleSheet } from 'react-native';
 import axios from 'axios';
 
 const App = () => {
@@ -33,7 +33,11 @@ const App = () => {
   );
 };
 
-//estilos
-
-
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+  },
+  
+})
 export default App;
